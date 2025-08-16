30e8e3b60f5a789cdf57c1178dc2157730c2e63d
Andrew Davis <jonandrewdavis@gmail.com>
2025-08-16 13:41:20 -0400

    trying random stuff

--- typescript-websockets-lobby/src/index.ts
+++ typescript-websockets-lobby/src/index.ts
@@ -1,4 +1,4 @@
-import WebSocket, { Server, WebSocketServer } from 'ws';
+import WebSocket, { Server, lobbyWebSocketServer } from 'ws';
 import * as crypto from 'crypto';
 import { DurableObject } from 'cloudflare:workers';
 import { ClientSocket } from './models/clientSocket';
@@ -10,7 +10,7 @@ import { ProtocolHelper } from './handlers/protocol-handler';
 const CONFIG_PORT = 80;
 
 export interface Env {
-	WEBSOCKET_SERVER: DurableObjectNamespace<WebSocketServer>;
+	WEBSOCKET_SERVER: DurableObjectNamespace<lobbyWebSocketServer>;
 	SECRET_KEY: string;
 }
 
@@ -41,7 +41,7 @@ export default {
 } satisfies ExportedHandler<Env>;
 
 // Durable Object
-export class WebSocketServer extends DurableObject {
+export class lobbyWebSocketServer extends DurableObject {
 	secretKey: string;
 	currentlyConnectedWebSockets: number;
 	gameServer: GameServerHandler;
